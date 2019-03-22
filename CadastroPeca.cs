6d7fdf61bbf0b02dd75a7e70dcaaf67869437515
WELISSON FRANGIOSI MIRANDA <47405945+mirandarp31@users.noreply.github.com>
2019-03-22 14:58:09 -0300

    Add files via upload

---
+++ CadastroPeca.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuPrincipal
+{
+    class CadastroPeca
+    {
+        public string nome;
+        public string categoria;
+        public string marca;
+        public decimal valor;
+
+        public void CadastrarBanco()
+        {
+
+        }
+    }
+}
